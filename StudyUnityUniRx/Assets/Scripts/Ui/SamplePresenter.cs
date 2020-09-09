c6a881c31324d864f939f8dcf43a50ab91d202a5
shimomuh <shimomuh0501@gmail.com>
2020-09-09 19:24:49 +0900

    FromCoroutine n WhenAll Sample

--- StudyUnityUniRx/Assets/Scripts/Ui/SamplePresenter.cs
+++ StudyUnityUniRx/Assets/Scripts/Ui/SamplePresenter.cs
@@ -1,4 +1,6 @@
-﻿using UniRx;
+﻿using System;
+using System.Collections;
+using UniRx;
 using UniRxSample;
 using UnityEditor;
 using UnityEngine;
@@ -14,7 +16,16 @@ namespace Ui
             AddAction("フレーム待ちなし", UnityWebRequestNoWait);
             AddAction("1フレーム待ち", UnityWebRequest1sWait);
             AddAction("10フレーム待ち", UnityWebRequest10sWait);
+            
+            AddLabel("FromCoroutineSample");
+            AddAction("処理の順序を理解する", FromCoroutineSample);
+            
+            AddLabel("WhenAllSample");
+            AddAction("ReturnUnit あり", WhenAllEitherReturnUnitSample);
+            AddAction("両方 FromCoroutine", WhenAllBothCorotineSample);
         }
+        
+        #region UnityWebRequest
         
         private void UnityWebRequestNoWait()
         {
@@ -49,6 +60,68 @@ namespace Ui
             RawImage image = canvas.gameObject.AddComponent<RawImage>();
             image.texture = texture;
             image.transform.localScale = new Vector2(0.01f, 0.01f);
+        }
+        
+        #endregion
+
+        private void FromCoroutineSample()
+        {
+            Observable.FromCoroutine(CoroutineSample, publishEveryYield: false)
+                .Subscribe(
+                    _ => Debug.Log("OnNext"),
+                    () => Debug.Log("OnCompleted")
+                ).AddTo(gameObject);
+        }
+
+        private IEnumerator CoroutineSample()
+        {
+            Debug.Log("Coroutine started.");
+
+            //なんか処理して待ち受ける的な
+            yield return new WaitForSeconds(3);
+
+            Debug.Log("Coroutine finished.");
+        }
+        
+        private void WhenAllEitherReturnUnitSample()
+        {
+            Observable.WhenAll(
+                Observable.FromCoroutine(_ => Coroutine5s()),
+                Observable.ReturnUnit().Do(_ => Debug.Log("超高速で流れるログ"))
+            )
+                .Subscribe(_ => Debug.Log("Subscribe!!"));
+        }
+
+        private void WhenAllBothCorotineSample()
+        {
+            Observable.WhenAll(
+                Observable.FromCoroutine(_ => Coroutine5s()),
+                Observable.FromCoroutine(_ => Coroutine2s())
+            )
+            .Subscribe(_ => Debug.Log("Subscribe!!"));
+
+        }
+
+        private IEnumerator Coroutine5s()
+        {
+            Debug.Log("5s start.");
+
+            //なんか処理して待ち受ける的な
+            yield return new WaitForSeconds(5);
+
+            Debug.Log("5s finished.");
+        }
+        
+        
+        private IEnumerator Coroutine2s()
+        {
+            Debug.Log("2s start.");
+
+            //なんか処理して待ち受ける的な
+            yield return new WaitForSeconds(2);
+
+            Debug.Log("2s finished.");
+            
         }
     }
 }
